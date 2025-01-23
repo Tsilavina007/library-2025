cfb92c84be7c52f41ef86e46845f562211df0d9e
Ryan Andriamahery <ryan@hei.school>
2025-01-23 09:29:20 +0300

    chore: add Author entity

---
+++ src/entity/Author.java
@@ -0,0 +1,36 @@
+package entity;
+
+import java.time.LocalDate;
+
+public class Author {
+    private String id;
+    private String name;
+    private LocalDate birthDate;
+
+    public Author() {
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
