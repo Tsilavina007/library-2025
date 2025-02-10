c78524eb1275ea17b942f19f57a3f1d51c2a22bb
tsilavina007 <andriamiart114@gmail.com>
2025-02-10 13:02:49 +0300

    ✨ feat: add mapper dao for sex

---
+++ src/dao/mapper/SexMapper.java
@@ -0,0 +1,19 @@
+package dao.mapper;
+
+import static entity.Sex.FEMALE;
+import static entity.Sex.MALE;
+
+import entity.Sex;
+
+public class SexMapper {
+    public Sex mapFromResultSet(String stringValue) {
+        if (stringValue == null) {
+            return null;
+        }
+        return switch (stringValue) {
+            case "MALE" -> MALE;
+            case "FEMALE" -> FEMALE;
+            default -> throw new IllegalArgumentException("Unknown Sex value " + stringValue);
+        };
+    }
+}
